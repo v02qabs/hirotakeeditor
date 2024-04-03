aff4b8c49353cdf312a186ab2735d28a6715f032
v02qabs <takesue090@gmail.com>
2024-04-03 18:15:07 +0900

    make-jhiroeditor

---
+++ jhirotakeeditor/hirotakeeditor.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+class hirotakeeditor{
+	public hirotakeeditor(){
+	}
+	private void start_editor(){
+		String hello = "ようこそ、hirotakeeditor へ.";
+		System.out.println(hello);
+	}
+}
